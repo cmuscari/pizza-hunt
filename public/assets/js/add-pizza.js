2cfef945de9cf162d547ca76fe6ee50503268931
Cortnie <cortniemuscari@gmail.com>
2022-06-01 23:23:24 -0500

    offline capabilities

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -67,6 +67,8 @@ const handlePizzaSubmit = event => {
     })
     .catch(err => {
       console.log(err);
+      saveRecord(formData);
+      // Now when a user tries to submit a new pizza without an internet connection, instead of the app simply failing, the data will be stored in the new_pizza object store in the browser's IndexedDB database called pizza_hunt!
     });
 };
 
